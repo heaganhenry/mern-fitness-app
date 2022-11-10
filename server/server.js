5c47ef2c690a1fb1d84bb7136cd078ae3f9f0ec1
heaganhenry <heaganhenry@gmail.com>
2022-11-10 17:57:47 -0400

    Setup mongoDB and mongoose

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 const app = express();
@@ -12,8 +13,15 @@ app.use((req, res, next) => {
     next();
 });
 
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
-});
+mongoose.connect(process.env.DB_URI)
+    .then(() => {
+        console.log('connected to db');
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port', process.env.PORT);
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
 
 app.use('/api/workouts', workoutRoutes);
