8b3e698c65a6ce5680f42f3e1f76542d4a5c774f
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-12-28 13:03:02 -0400

    Move Navbar outside pages div

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import Home from './pages/Home';
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <div className="pages">
-        <Navbar />
         <Routes>
           <Route
             path="/"
