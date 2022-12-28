12abcea60d676e470691b9aff293aec63cf87d26
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-12-28 12:52:33 -0400

    Send empty fields for styles and improve error message

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -23,6 +23,22 @@ const getWorkout = async (req, res) => {
 const createWorkout = async (req, res) => {
     const { title, reps, weight } = req.body;
 
+    const emptyFields = [];
+
+    if (!title) {
+        emptyFields.push('title');
+    }
+    if (!reps) {
+        emptyFields.push('reps');
+    }
+    if (!weight) {
+        emptyFields.push('weight');
+    }
+
+    if (emptyFields.length > 0) {
+        return res.status(400).json({ error: 'Please fill in all the fields', emptyFields });
+    }
+
     try {
         const workout = await Workout.create({ title, reps, weight });
         res.status(200).json(workout);
