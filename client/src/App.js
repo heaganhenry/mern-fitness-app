eae7904747bc5198dd1f4045e8933eceafc28577
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-12-28 12:34:06 -0400

    Add Navbar and Home to root App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="pages">
+        <Navbar />
         <Routes>
           <Route
+            path="/"
+            element={<Home />}
           />
         </Routes>
       </div>
