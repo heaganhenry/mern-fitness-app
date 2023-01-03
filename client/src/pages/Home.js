75aea22faa2d156dd26d40217cd13aead2e0f47d
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2023-01-03 13:49:35 -0400

    Send auth token with request from Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,18 @@
 import { useEffect } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
     const { workouts, dispatch } = useWorkoutsContext();
+    const { user } = useAuthContext();
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts');
+            const response = await fetch('/api/workouts', {
+                headers: { 'Authorization': `Bearer ${user.token}` }
+            });
             const json = await response.json();
 
             if (response.ok) {
@@ -17,7 +21,7 @@ const Home = () => {
         };
 
         fetchWorkouts();
-    }, [dispatch]);
+    }, [dispatch, user]);
 
     return (
         <div className="home">
