a39ea3e51f26aaad7ed7f565748cc4023768c5c0
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2023-01-03 12:53:26 -0400

    Register requireAuth to protect workout routes

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const workoutController = require('../controllers/workoutController');
+const requireAuth = require('../middleware/requireAuth');
 
 const router = express.Router();
+
+router.use(requireAuth);
 
 router.get('/', workoutController.getWorkouts);
 
